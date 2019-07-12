1fc6b75da0e8cb402aeee0296294aa5881eb89ef
dikspr <dikspr@gmail.com>
2019-07-12 14:38:12 +0300

    закончил первый модуль

--- Homework6/music_serialize.py
+++ Homework6/music_serialize.py
@@ -10,9 +10,22 @@
 # вывести результаты в терминал. Записать результаты в файлы group.json, group.pickle соответственно.
 # В файле group.json указать кодировку utf-8.
 
+import pickle
+import json
+
 my_favourite_group = {
     'name': 'Г.М.О.',
     'tracks': ['Последний месяц осени', 'Шапито'],
     'Albums': [{'name': 'Делать панк-рок','year': 2016},
     {'name': 'Шапито','year': 2014}]}
+
+with open('group.pickle', 'wb') as f:
+    pickle.dump(my_favourite_group, f)
+
+print ('group.pickle записан')
+
+with open('group.json', 'w') as f1:
+    json.dump(my_favourite_group, f1)
+
+print ('group.json записан')
 
