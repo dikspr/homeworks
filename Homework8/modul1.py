30469d6a085a21aac489f615dd489deaebf205ee
dikspr <dikspr@gmail.com>
2019-07-14 23:05:14 +0300

    закончен модуль

--- Homework8/modul1.py
+++ Homework8/modul1.py
@@ -1,4 +1,6 @@
 import os
+import shutil
+import datetime
 
 
 # Создание файла
@@ -17,8 +19,41 @@ def create_dir(name):
         print('Такая папка уже есть')
 
 
+# получение списка директорий
+def get_list(folders_only=False):
+    result = os.listdir()
+    if folders_only:
+        result = [f for f in result if os.path.isdir(f)]
+    print(result)
+
+
+# удаление файла или директории
+def delete_file(name):
+    if os.path.isdir(name):
+        os.rmdir(name)
+    else:
+        os.remove(name)
+
+
+# копирование файла или директории
+def copy_file(name, new_name):
+    if os.path.isdir(name):
+        try:
+            shutil.copytree(name, new_name)
+        except FileExistsError:
+            print('Такая папка уже есть')
+    else:
+        shutil.copy(name, new_name)
+# запись лога
+def save_info(message):
+    current_time = datetime.datetime.now()
+    result = f'{current_time} - {message}'
+    with open('log.txt', 'a', encoding='utf-8') as f:
+        f.write(result + '\n')
+
 
 if __name__ == '__main__':
     create_file('text.dat')
     create_file('text.dat', 'some text')
     create_dir('new_f')
+    get_list(True)
